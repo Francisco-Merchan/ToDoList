53f2ee1ac9c02b41527d3ed1aada6b3c75976a2b
FranMerchan <merchanf6@gmail.com>
2021-07-22 19:28:59 -0300

    ToDoList terminado, falta hacerlo responsive

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const inputForm = document.querySelector("input");
 const listaTareas = document.querySelector("#lista_tareas");
 const button = document.querySelector(".btn-info");
 let tareas = [];
+let idG;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (inputForm.value !== "") {
-    let tarea = inputForm.value.toUpperCase();
-    createObjects(tarea);
+  if (button.value === "EDITAR") {
+    console.log("hola");
+    let tareaNueva = inputForm.value;
+    tareas = tareas.map((tarea) => {
+      if (tarea.id == idG) {
+        return {
+          ...tarea,
+          tarea: tareaNueva,
+        };
+      } else {
+        return tarea;
+      }
+    });
+  } else {
+    if (inputForm.value !== "") {
+      let tarea = inputForm.value.toUpperCase();
+      createObjeto(tarea);
+    }
   }
   localStorage.setItem("tareas", JSON.stringify(tareas));
+  button.value = "AGREGAR";
+  form.reset();
   mostrarEnDOM();
 });
 
-function createObjects(tarea) {
+function createObjeto(tarea) {
   const newTarea = {
     tarea,
     status: "PENDIENTE",
@@ -88,16 +106,16 @@ function limpiarDOM() {
 listaTareas.addEventListener("click", (e) => {
   e.preventDefault();
   if (e.target.classList.contains("bi-trash-fill")) {
-    const id = e.target.parentElement.parentElement.id;
-    borrarStorage(id);
+    idG = e.target.parentElement.parentElement.id;
+    borrarStorage(idG);
   }
   if (e.target.classList.contains("bi-arrow-repeat")) {
-    const id = e.target.parentElement.parentElement.id;
-    cambiarStatus(id);
+    idG = e.target.parentElement.parentElement.id;
+    cambiarStatus(idG);
   }
   if (e.target.classList.contains("bi-pencil-fill")) {
-    const id = e.target.parentElement.parentElement.id;
-    editarStorage(id);
+    idG = e.target.parentElement.parentElement.id;
+    editarStorage(idG);
   }
 });
 
@@ -109,9 +127,9 @@ function borrarStorage(id) {
 
 function cambiarStatus(id) {
   let posicion = tareas.findIndex((tarea) => tarea.id == id);
-  if ((tareas[posicion].status = "PENDIENTE")) {
-    tareas[posicion].status = "REALIZADA";
-  } else if ((tareas[posicion].status = "REALIZADA")) {
+  if (tareas[posicion].status === "REALIZADA") {
+    tareas[posicion].status = "PENDIENTE";
+  } else {
     tareas[posicion].status = "REALIZADA";
   }
   localStorage.setItem("tareas", JSON.stringify(tareas));
@@ -120,7 +138,8 @@ function cambiarStatus(id) {
 
 function editarStorage(id) {
   const elemento = tareas.find((tarea) => tarea.id == id);
-  console.log(elemento);
   inputForm.value = elemento.tarea;
   button.value = "EDITAR";
 }
+
+document.addEventListener("DOMContentLoaded", mostrarEnDOM);
